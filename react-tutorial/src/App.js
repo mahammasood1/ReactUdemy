71e9f16c23772a97b98570b9eec12ba019025800
Maham <mahammasood31@gmail.com>
2019-07-30 17:54:38 +0500

    latest commit

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-
+import './App.css'
 class App extends Component {
+
+  state = {
+    counter: 0
+  }
+
+  increment = () => {
+    this.setState({counter:5})
+  }
+
   render() {
     return (
       <div className="App">
-        Welcome to React!
+
+        <button onClick={()=>{this.increment()}}>Increment</button>
+        <div>
+          Div 1: {this.state.counter}
+        </div>
+
+        <div>
+          Div 2:
+        </div>
+
+        <div>
+          Div 3:
+        </div>
     </div>
     );
   }
